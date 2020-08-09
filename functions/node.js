59f5b9d343e4ba569830f28067b35b4809ca7021
Emma Goodliffe <emma@piemag.com>
2020-08-09 15:44:14 +0100

    ready for functions/node.js

--- functions/node.js
+++ functions/node.js
@@ -7,4 +7,3 @@ const relPathToAbs = path => resolve(dirname(""), path);
 const relGoogleKeyFilename = "./lgim-stt-key.json";
 const absGoogleKeyFilename = relPathToAbs(relGoogleKeyFilename);
 process.env.GOOGLE_APPLICATION_CREDENTIALS = absGoogleKeyFilename;
-
