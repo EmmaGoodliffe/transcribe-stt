f25de568673274baee0c2c85eb033330cfa27227
Emma Goodliffe <emma@piemag.com>
2020-08-10 13:39:39 +0100

    ready for promise refactoring

---
+++ functions/node.ts
@@ -0,0 +1,91 @@
+import { dirname, resolve } from "path";
+import { createReadStream, appendFile, writeFileSync } from "fs";
+import { SpeechClient } from "@google-cloud/speech";
+import * as ora from "ora";
+
+// Helpers
+const relPathToAbs = (path: string) => resolve(dirname(""), path);
+
+// Prepare environment
+const relGoogleKeyFilename = "./lgim-stt-key.json";
+const absGoogleKeyFilename = relPathToAbs(relGoogleKeyFilename);
+process.env.GOOGLE_APPLICATION_CREDENTIALS = absGoogleKeyFilename;
+
+// Define constants
+const SPINNER_START_TEXT = "STT stream running...";
+const SPINNER_STOP_TEXT = "STT stream done";
+
+class STTStream {
+  audioFilename: string;
+  textFilename: string;
+  sampleRateHertz: number;
+  append: boolean;
+  spinner: ora.Ora;
+  constructor(
+    audioFilename: STTStream["audioFilename"],
+    textFilename: STTStream["textFilename"],
+    sampleRateHertz: STTStream["sampleRateHertz"],
+    append = false
+  ) {
+    this.audioFilename = audioFilename;
+    this.textFilename = textFilename;
+    this.sampleRateHertz = sampleRateHertz;
+    this.append = append;
+    this.spinner = ora(SPINNER_START_TEXT);
+  }
+  start() {
+    this.spinner.start();
+    // If not appending
+    if (!this.append) {
+      // Empty file
+      writeFileSync(this.textFilename, "");
+    }
+
+    // Initialise client
+    const client = new SpeechClient();
+
+    // Define request
+    const request = {
+      config: {
+        encoding: "MP3" as const, // "LINEAR16" as const,
+        sampleRateHertz: this.sampleRateHertz,
+        languageCode: "en-GB",
+      },
+      // interimResults: true,
+    };
+
+    // Create read stream for audio file
+    const audioReadStream = createReadStream(this.audioFilename);
+
+    // Define a read/write stream to handle audio file
+    const recogniseStream = client
+      .streamingRecognize(request)
+      .on("error", err => {
+        // Handle errors
+        throw `Error in STT stream: ${err}`;
+      })
+      .on("data", data => {
+        // Get result
+        const result = data.results[0].alternatives[0].transcript as string;
+        // Append result to text file
+        appendFile(this.textFilename, `${result}\n`, () => {});
+      })
+      .on("end", () => {
+        this.spinner.succeed(SPINNER_STOP_TEXT);
+      });
+
+    // Pipe audio file through read/write stream
+    audioReadStream.pipe(recogniseStream);
+  }
+}
+
+// Define filenames
+const shortAudioFilename = "test mono";
+const shortTextFilename = "text";
+const audioFilename = `audio/${shortAudioFilename}.wav`;
+const textFilename = `${shortTextFilename}.txt`;
+
+// Initialise STT stream
+const sttStream = new STTStream(audioFilename, textFilename, 48000);
+// Start STT stream
+sttStream.start();
