e016ddcf1905b921e806603c830d75aa2db89e25
Emma Goodliffe <emma@piemag.com>
2020-08-16 09:34:41 +0100

    renamed

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as STTStream } from "./STTStream";
-export { default as DistributedSTTStream } from "./DistributedSTTStream";
-export * from "./types";
-//# sourceMappingURL=index.d.ts.map
