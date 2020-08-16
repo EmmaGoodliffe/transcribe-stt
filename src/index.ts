1713eab50d6c8611fe6f287f2a50c56b56911dd2
Emma Goodliffe <emma@piemag.com>
2020-08-16 10:06:27 +0100

    added package documentation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * @packageDocumentation
+ * Transcribe audio of any length using Google's Speech to Text API
+ */
+
 export { default as STTStream } from "./STTStream";
 export { default as DistributedSTTStream } from "./DistributedSTTStream";
 export * from "./types";
