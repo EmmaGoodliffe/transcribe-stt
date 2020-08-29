2589d6fd2a724b7662bb79fecc0a2f0d77e10908
Emma Goodliffe <emma@piemag.com>
2020-08-29 16:33:09 +0100

    post-process exports lint fully done

--- scripts/ts/exports/index.ts
+++ scripts/ts/exports/index.ts
@@ -9,7 +9,7 @@ const fixing = argv.fix;
 const main = async () => {
   const unused = (await findUnused(baseDirname)).nonIndexUnused;
   if (!unused.length) {
-    console.log("No unused exports");
+    console.log("All exports used");
     return;
   }
   const reasons: string[] = [];
